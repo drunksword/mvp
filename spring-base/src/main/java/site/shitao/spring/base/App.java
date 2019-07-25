b95cec6c17714b57173d430dea2efd5f928b9730
shitao <tao.shi@ximalaya.com>
2019-07-25 09:26:49 +0800

    add spring base

---
+++ spring-base/src/main/java/site/shitao/spring/base/App.java
@@ -0,0 +1,20 @@
+package site.shitao.spring.base;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Author: tao.shi@ximalaya.com
+ * Created: 7/25
+ */
+public class App {
+
+    public static void main(String... args){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
+
+        TestBean bean = (TestBean) applicationContext.getBean("shitao");
+
+        System.out.println(String.format("\n>>>>>>\n\n%s\n\n>>>>>>>>\n", JSON.toJSONString(bean)));
+    }
+}
---
+++ spring-base/src/main/java/site/shitao/spring/base/TestBean.java
@@ -0,0 +1,13 @@
+package site.shitao.spring.base;
+
+import lombok.Data;
+
+/**
+ * Author: tao.shi@ximalaya.com
+ * Created: 7/25
+ */
+@Data
+public class TestBean {
+    String name;
+    String job;
+}
